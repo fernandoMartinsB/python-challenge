76ba1c0a7b090c6009be037d1b9eb125a98bd115
Fernando M Barbosa <fernando.martins.info@gmail.com>
2020-02-13 09:53:43 -0300

    add index page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render 
 
-# Create your views here.
+""" Long tearm template request """
+# def index(request):
+#     return HttpResponse(loader.get_template('core/index.html').render({},request))
+
+""" Short tearm template request"""
 def index(request):
-    return HttpResponse('Hello from python')
+    return render(request, 'core/index.html', {})
