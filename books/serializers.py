c9301ae383fd8f1487e68fd2ed8216e851b37d75
Fernando.Barbosa <fernando.barbosa@rentcars.com>
2020-05-11 18:57:07 -0300

    update the return of route books to use serializers.ModelSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -1,28 +1,23 @@
 from rest_framework import serializers
 from .models import Book, Author, Publisher
 
-class BookSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
-    publisher = serializers.StringRelatedField()
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publisher']
-
-
-
-
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['name', 'nationality']
 
 
-
-
-class PublisherSerializer(serializers.HyperlinkedModelSerializer):
+class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publisher
         fields = ['name', 'cowntry']
 
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer();
+    publisher = PublisherSerializer();
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publisher']
 
 
